eb6613341cdcc0c9b94fb33454b5af0fe42bb4d3
Joshua Tate <jmtate@icloud.com>
2017-12-21 15:42:51 -0700

    Started Chapter 9: Added Checklists project

---
+++ Checklists/Checklists/ChecklistTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ChecklistTableViewController.swift
+//  Checklists
+//
+//  Created by Joshua Tate on 2017-12-21.
+//  Copyright © 2017 Magk. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistTableViewController: UITableViewController {
+}
