02fc4b0468f4116ea52e9c4fb8109c310a60a59b
Joshua Tate <jmtate@icloud.com>
2018-01-02 15:50:36 -0700

    Started Chapter 23: Added location error checking

--- MyLocations/MyLocations/CurrentLocationViewController.swift
+++ MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -13,6 +13,8 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     // MARK: - Properties
     private let locationManager = CLLocationManager()
     private var location: CLLocation?
+    private var updatingLocation = false
+    private var lastLocationError: Error?
     // MARK: - Outlets
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
@@ -28,11 +30,19 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     // MARK: - Location manager delegate
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("didFailWithError: \(error)")
+        // When locationUnknown, location manager is still attempting to find location
+        if (error as NSError).code == CLError.locationUnknown.rawValue {
+            return
+        }
+        lastLocationError = error
+        stopLocationManager()
+        updateLabels()
     }
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let lastLocation = locations.last!
         print("didUpdateLocations: \(lastLocation)")
         location = lastLocation
+        lastLocationError = nil
         updateLabels()
     }
     // MARK: - Actions
@@ -48,9 +58,8 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
             showLocationServicesDeniedAlert()
             return
         }
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-        locationManager.startUpdatingLocation() // Begin receiving coordinates
+        startLocationManager()
+        updateLabels()
     }
     // MARK: - Methods
     private func showLocationServicesDeniedAlert() {
@@ -73,7 +82,36 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
             latitudeLabel.text = ""
             longitudeLabel.text = ""
             tagButton.isHidden = true
-            messageLabel.text = "Tap 'Get My Location' to Start"
+            let statusMessage: String
+            if let error = lastLocationError as NSError? {
+                if error.domain == kCLErrorDomain && error.code == CLError.denied.rawValue {
+                    statusMessage = "Location Services Disabled."
+                } else {
+                    statusMessage = "Error Getting Location."
+                }
+            } else if !CLLocationManager.locationServicesEnabled() {
+                statusMessage = "Location Services Disabled."
+            } else if updatingLocation {
+                statusMessage = "Searching..."
+            } else {
+                statusMessage = "Tap 'Get My Location' Button to Start."
+            }
+            messageLabel.text = statusMessage
+        }
+    }
+    private func startLocationManager() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation() // Begin receiving coordinates
+            updatingLocation = true
+        }
+    }
+    private func stopLocationManager() {
+        if updatingLocation {
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            updatingLocation = false
         }
     }
 }
