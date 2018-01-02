9484c517e33b294c2393d66b5c562238d2819461
Joshua Tate <jmtate@icloud.com>
2018-01-01 21:36:34 -0700

    Started Chapter 22: Created MyLocations project

---
+++ MyLocations/MyLocations/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Joshua Tate on 2018-01-01.
+//  Copyright © 2018 Magk. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
